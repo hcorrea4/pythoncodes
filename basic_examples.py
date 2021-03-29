b0b02eaa04171702b22beeb236d09fcc320b74ed
Humberto Correa <81060316+hcorrea4@users.noreply.github.com>
2021-03-28 22:08:16 -0400

    Add files via upload

---
+++ basic_examples.py
@@ -0,0 +1,53 @@
+msg_1 ="Hola"
+msg_2 ="Pepe"
+print(msg_1 == msg_2)
+msg =  "Humberto Correa"
+print(msg.find("a"))
+num1 = 5
+num2 = 6
+resultado = num1 + num2
+print(resultado)
+
+print("suma: ")
+num_1 = 5
+num_2 = 50
+resultado = num_1 + num_2
+print("Resultado de la suma: ", str(resultado))
+
+print("resta: ")
+num_1 = 5
+num_2 = 50
+resultado = num_1 - num_2
+print("Resultado de la resta: ", str(resultado))
+
+print("multiplicacion: ")
+num_1 = 5
+num_2 = 500
+resultado = num_1 * num_2
+print("Resultado de la multiplicacion: ", str(resultado))
+
+print("modulo: ")
+num_1 = 5
+num_2 = 500
+resultado = num_1 % num_2
+print("Resultado de la modulo: ", str(resultado))
+
+import math as r
+print("raiz cuadrada: ")
+num_2 = 500
+resultado = r.sqrt(num_2)
+print("Resultado de la raiz cuadrada: ", str(resultado))
+
+print("potencia: ")
+base = 2
+exponente = 5
+resultado = r.pow(base,exponente)
+resultado_no_math = base ** exponente
+print("Resultado de la potencia(lib math): ", str(resultado), " resultado sin lib math ", str(resultado_no_math))
+
+print("division: ")
+dividendo = 20
+divisor = 5
+resultado =
+dividendo / divisor
+print("Resultado de la division: ", str(resultado))
